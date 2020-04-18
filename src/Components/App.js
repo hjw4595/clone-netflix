4c36113edd9eb1d0b94191b39d6b9f1fae6ad6b7
hjw4595 <hjw4595@naver.com>
2020-04-18 23:28:19 +0900

    css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Router from "./Router"
-import Header from "./Header"
+import Header from "./Header/index"
 class App extends Component {
   render(){
     return (
