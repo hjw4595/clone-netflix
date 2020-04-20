8c298517fba59fb8cc74dcd4b9b3ae183cc7b24d
hjw4595 <hjw4595@naver.com>
2020-04-20 23:06:15 +0900

    axios

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {Link, withRouter} from "react-router-dom"
 import styled from "styled-components";
 
 const List = styled.ul`
@@ -9,39 +9,41 @@ const Item = styled.li`
     width: 80px;
     height: 50px;
     text-align: center;
-`
+    border-bottom:3px solid ${props => props.selected ? "green" : "transparent"};
+    transition: border-bottom .5s ease-in-out;
+`;
 const Header = styled.header`
     color: white;
     position: fixed;
     top: 0;
-    left: 0;
-    width: 100%;
+    left: 0; width: 100%;
     height: 50px;
     display: flex;
     align-items: center;
     background-color: rgba(20, 20, 20, 0.8);
     z-index: 10;
-    box-shadow: 0px 1px 5px 2px rgba(255, 255, 255, 0.8);
+    box-shadow: 0px 1px 5px 2px rgba(0, 150, 150, 0.8);
 `;
 
 const Hlink = styled(Link)`
     height: 50px;
-    display: flex;
+    display: flex ;
     align-items: center;
     justify-content: center;
-`
-export default () => (
-    <Header>
+`;
+export default withRouter(({location : {pathname} }) => (
+    <Header> 
         <List>
-            <Item>
+            <Item selected ={pathname ==="/"}> 
                 <Hlink to="/">Movies</Hlink>
             </Item>
-            <Item>
+            <Item selected ={pathname ==="/TV"}>
                 <Hlink to="/TV">TV</Hlink>
             </Item>
-            <Item>
+            <Item selected ={pathname ==="/Search"}>
                 <Hlink to="/Search">Search</Hlink>
             </Item>
         </List>
     </Header>
     )
+)
