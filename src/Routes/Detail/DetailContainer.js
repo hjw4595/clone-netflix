79cff1687293de873f79d3064f1d0a0cc3fe9866
hjw4595 <hjw4595@naver.com>
2020-04-25 03:06:04 +0900

    presenter / Section

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,24 +1,51 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
-export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+import { moviesApi, tvApi } from "../../api";
 
-  // async componentDidMount() {
-  //   const {
-  //     match: {
-  //       params: { id }
-  //     },
-  //     history: { push }
-  //   } = this.props;
-  //   const parsedId = parseInt(id);
-  //   if (isNaN(parsedId)) {
-  //     return push("/");
-  //   }
-  // }
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname }
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = this.props;
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+    //  this.isMovie = pathname.includes("/movie/")
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+    let result = null;
+    try {
+      if (isMovie) {(
+        {data : result } = await moviesApi.movieDetail(parsedId)
+        );
+      } else {(
+        {data : result } = await tvApi.showDetail(parsedId)
+       );
+
+      }
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
+  
 
   render() {
     console.log(this.props)
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,32 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "../../Components/Section";
+
+const Container = styled.div`
+    padding : 0 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
+    loading ? error : 
+    <Container>
+        {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="nowPlaying">{nowPlaying.map(movie => movie.title)}</Section>
+        )}
+                {popular && popular.length > 0 && (
+        <Section title="popular">{popular.map(movie => movie.title)}</Section>
+        )}
+                {upcoming && upcoming.length > 0 && (
+        <Section title="upcoming">{upcoming.map(movie => movie.title)}</Section>
+        )}
+    </Container>
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  popular: PropTypes.array,
+  upcomfing: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string
+};
+
+export default HomePresenter;
